0560f2b752a36a11c8c678c6beabb1b8a282b894
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-16 17:50:29 -0500

    random: Initial commit.

---
+++ micropython/brightness.py
@@ -0,0 +1,13 @@
+from microbit import *
+
+brightness = Image(
+"13531:"
+"35753:"
+"57975:"
+"35753:"
+"13531")
+
+while True:
+	display.show(brightness)
+	sleep(100)
+
---
+++ micropython/random_pattern.py
@@ -0,0 +1,22 @@
+from microbit import *
+from random import *
+
+def combine(array, string):
+	return string.join(str(element) for element in array)
+
+def array2image(array):
+	return combine([combine(row,'') for row in array], ':')
+
+def array2image2(array):
+	dummy = []
+	for row in array:
+		dummy.append(combine(row, ''))
+	return combine(dummy, ':')
+
+while True:
+	rand = [[randint(0,9) for i in range(5)] for i in range(5)]
+	string =  array2image2(rand)
+	image = Image(string)
+	display.show(image)
+	sleep(100)
+
