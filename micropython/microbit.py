5797a4c70982e4015863a01e37d07fbb1f6ae9ae
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-20 23:30:00 -0500

    timer: Initial commit.

--- micropython/microbit.py
+++ micropython/microbit.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 def sleep(ms):
 	time.sleep(ms/1000)
@@ -11,4 +12,9 @@ class Display:
 		print(image)
 
 display = Display()
+
+
+microbit_start_time = datetime.now()
+def running_time():
+	return (datetime.now() - microbit_start_time).microseconds
 
---
+++ micropython/timer.py
@@ -0,0 +1,23 @@
+from microbit import *
+from random import *
+
+digit = []
+digit += ["09990:09090:09090:09090:09990"]
+digit += ["00900:09900:00900:00900:09990"]
+digit += ["09990:00090:09990:09000:09990"]
+digit += ["09990:00090:09990:00090:09990"]
+digit += ["09090:09090:09990:00090:00090"]
+digit += ["09990:09000:09990:00090:09990"]
+digit += ["09990:09000:09990:09090:09990"]
+digit += ["09990:00090:00090:00900:00900"]
+digit += ["09990:09090:09990:09090:09990"]
+digit += ["09990:09090:09990:00090:09990"]
+
+start_time = running_time()
+while True:
+	seconds = int(round((running_time() - start_time)/1000))
+	string = digit[seconds % 10]
+	display.show(Image(string))
+	sleep(1000)
+
+
