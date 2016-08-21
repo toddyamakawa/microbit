6fd76ea21314c021fe6b3f0497fcc7b03300f7f2
Todd Yamakawa <todd.yamakawa@arm.com>
2016-08-20 22:43:41 -0500

    lines: Fixing divide by zero bug.

--- micropython/lines.py
+++ micropython/lines.py
@@ -1,16 +1,16 @@
 from microbit import *
 from random import *
 
-def line(x1, y1, x2, y2):
+def line(row1, col1, row2, col2):
 	points = []
-	m = (0.0 + y2 - y1) / (0.0 + x2 - x1)
-	b = y1 - m * x1
-	for x in range(min(x1,x2), max(x1,x2)+1):
-		y = int(round(m * x + b))
-		if y in range(5): points += [[x, y]]
-	for y in range(min(y1,y2), max(y1,y2)+1):
-		x = int(round((y - b)/m))
-		if x in range(5): points += [[x, y]]
+	m = (0.01 + col2 - col1) / (0.01 + row2 - row1)
+	b = col1 - m * row1
+	for row in range(min(row1,row2), max(row1,row2)+1):
+		col = int(round(m * row + b))
+		if col in range(5): points += [[row, col]]
+	for col in range(min(col1,col2), max(col1,col2)+1):
+		row = int(round((col - b)/m))
+		if row in range(5): points += [[row, col]]
 	return points
 
 def show(points):
@@ -21,15 +21,21 @@ def show(points):
 	image = Image(string)
 	display.show(image)
 
+speed = 1000
 while True:
-	show(line(0, 0, 2, 1))
-	sleep(1000)
-	show(line(0, 0, 1, 2))
-	sleep(1000)
-	show(line(4, 4, 0, 0))
-	sleep(1000)
-	show(line(0, 0, 1, 4))
-	sleep(1000)
-	#display.show(current)
-	# exit(0)
+	for col in range(2,5):
+		show(line(2, 2, 0, col))
+		sleep(speed)
+	for row in range(1,5):
+		show(line(2, 2, row, 4))
+		sleep(speed)
+	for col in range(3,0,-1):
+		show(line(2, 2, 4, col))
+		sleep(speed)
+	for row in range(4,0,-1):
+		show(line(2, 2, row, 0))
+		sleep(speed)
+	for col in range(0,1):
+		show(line(2, 2, 0, col))
+		sleep(speed)
 
