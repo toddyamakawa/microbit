6e3de898e85e0032f626c8538279aa90950e0829
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-23 17:06:41 -0500

    microbit: Updating Display.show()

--- micropython/microbit.py
+++ micropython/microbit.py
@@ -2,14 +2,20 @@ import time
 from datetime import datetime
 
 def sleep(ms):
-	time.sleep(ms/1000)
+	time.sleep(ms/1000.0)
 
 def Image(string):
 	return string
 
 class Display:
 	def show(self, image):
-		print(image)
+		print("\033c")
+		print('+'+'-'*11+'+')
+		for row in image.split(':'):
+			row = ' '.join(list(row.replace('0', ' ')))
+			print('| ' + row + ' |')
+		print('+'+'-'*11+'+')
+		sleep(100)
 
 display = Display()
 
