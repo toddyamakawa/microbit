3a665f178480ddf6d0aad92b067bdc3798b64442
Todd Yamakawa <todd.yamakawa@arm.com>
2016-09-23 17:47:45 -0500

    microbit: Adding color to Display.show()

--- micropython/microbit.py
+++ micropython/microbit.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from datetime import datetime
 
@@ -9,12 +10,35 @@ def Image(string):
 
 class Display:
 	def show(self, image):
-		print("\033c")
-		print('+'+'-'*11+'+')
+
+		# Colors
+		reset = "\033[0m"
+		bright = "\033[101;91m"
+		dim = "\033[41;31m"
+		off = "\033[8m"
+		clear = "\033c"
+
+		# Display format
+		width = 2
+		empty_row = '|' + ' '*(5*(1+width)+1) + "|\n"
+		header_row = '+' + '-'*(5*(1+width)+1) + "+\n"
+
+		sys.stdout.write(clear)
+		sys.stdout.write(header_row)
 		for row in image.split(':'):
-			row = ' '.join(list(row.replace('0', ' ')))
-			print('| ' + row + ' |')
-		print('+'+'-'*11+'+')
+			sys.stdout.write('| ')
+			for col in list(row):
+				if(int(col) == 0):
+					col = off + col + reset
+				elif(int(col) >= 6):
+					col = bright + col + reset
+				else:
+					col = dim + col + reset
+				sys.stdout.write(col*width + ' ')
+			sys.stdout.write("|\n")
+			sys.stdout.write(empty_row)
+		sys.stdout.write(header_row)
+		sys.stdout.flush
 		sleep(100)
 
 display = Display()
