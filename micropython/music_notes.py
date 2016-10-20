fa0a800fca0a79f4e539ab60dbfeb4f8b2815e0f
Todd Yamakawa <todd.yamakawa@arm.com>
2016-10-19 22:05:25 -0500

    music_notes: Adding accelerometer lookup table.

--- micropython/music_notes.py
+++ micropython/music_notes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from microbit import *
+import music
 
 letters = {}
 letters['A'] = "09000:90900:99900:90900:90900"
@@ -12,11 +13,28 @@ letters['G'] = "99900:90000:90000:90900:99900"
 
 octave = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
+tilt_xp = []
+tilt_xp += [[ 300, 'C']]
+tilt_xp += [[ 500, 'D']]
+tilt_xp += [[ 600, 'E']]
+tilt_xp += [[ 700, 'F']]
+tilt_xp += [[ 900, 'G']]
+tilt_xp += [[ 950, 'A']]
+tilt_xp += [[1000, 'B']]
+
 start_time = running_time()
 while True:
-	seconds = int(round((running_time() - start_time)/1000))
-	string = letters[octave[seconds % 7]]
-	display.show(Image(string))
-	sleep(10)
+	x = accelerometer.get_x()
+	y = accelerometer.get_y()
 
+	for t in tilt_xp:
+		if x < t[0]:
+			note = t[1]
+			break
+
+	string = letters[note]
+	display.show(Image(string))
+
+	if(button_a.get_presses() != 0):
+		music.play([note + '4:4'])
 
